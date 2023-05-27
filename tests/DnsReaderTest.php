e33665d647b54e0190b353f4d0e42eb7b840345e
nazaryanenko <nazar@electricwebmarketing.com>
2023-05-27 16:21:22 +0300

    todo app

--- tests/DnsReaderTest.php
+++ tests/DnsReaderTest.php
@@ -1,8 +1,26 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Collection;
+use Nazaryanenko\Dnsreader\DnsReaderService;
+use Orchestra\Testbench\TestCase;
 
 class DnsReaderTest extends TestCase
 {
+    public function testGetDnsRecordsReturnsEmptyCollectionWhenCheckRecordFails()
+    {
+        $dnsReaderService = new DnsReaderService();
+        $result = $dnsReaderService->getDnsRecords('https://invalidhostname.com');
 
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertTrue($result->isEmpty());
+    }
+
+    public function testGetDnsRecordsReturnsCollectionOfRecordsWhenCheckRecordSucceeds()
+    {
+        $dnsReaderService = new DnsReaderService();
+        $result = $dnsReaderService->getDnsRecords('https://google.com');
+
+        $this->assertInstanceOf(Collection::class, $result);
+        $this->assertTrue($result->isNotEmpty());
+    }
 }
