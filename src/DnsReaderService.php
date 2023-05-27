16e9eb2fc776e1f3cba7daa3d7297b5b00175912
nazaryanenko <nazar@electricwebmarketing.com>
2023-05-27 17:09:20 +0300

    todo app

--- src/DnsReaderService.php
+++ src/DnsReaderService.php
@@ -3,25 +3,15 @@
 namespace Nazaryanenko\Dnsreader;
 
 use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
 
 class DnsReaderService
 {
     public function getDnsRecords(string $hostName): Collection
     {
-        $hostName = $this->resolveHostName($hostName);
-
-        if (!$records = dns_get_record($hostName)) {
+        if (!dns_check_record($hostName)) {
             return Collection::make();
         }
 
-        return Collection::make($records);
-    }
-
-    protected function resolveHostName(string $hostName): string
-    {
-        return Str::of($hostName)
-                  ->match('/(?<=\/\/)([\w.-]+)(?=[?\/]|$)/');
-
+        return Collection::make(dns_get_record($hostName));
     }
 }
